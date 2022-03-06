c5be2c4f8e6498e2e2585232a3b05ccd5ec4862a
Jatin Vaidya <jatin.vaidya@gmail.com>
2022-03-06 17:07:22 +1100

    test ready

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,9 +5,11 @@ import logo from "../assets/logo.svg";
 const Hero = () => (
 	<div className="text-center hero my-5">
 		<img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-		<h1 className="mb-4">app.netenrich.com</h1>
+		<h1 className="mb-4">Google Chronicle instance for Vodafone</h1>
 
-		<p className="lead">This is a sample application for app.netenrich.com</p>
+		<p className="lead">
+			This is a fake instance of Google Chronicle for NE customer Vodafone
+		</p>
 	</div>
 );
 
