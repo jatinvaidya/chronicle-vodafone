7e510cd40805113a84bd87c418dd0fa8e6b62b02
Jatin Vaidya <jatin.vaidya@gmail.com>
2022-03-06 21:24:12 +1100

    search params

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,10 +4,12 @@ const Login = () => {
 	const { loginWithRedirect } = useAuth0();
 	let search = window.location.search;
 	let searchParams = new URLSearchParams(search);
-	const organization = searchParams.get("organization");
+	/*const organization = searchParams.get("organization");
 	const invitation = searchParams.get("invitation");
+	const connection = searchParams.get("connection");
 	if (!!organization && !!invitation)
-		loginWithRedirect({ organization, invitation });
+		loginWithRedirect({ organization, invitation });*/
+	loginWithRedirect(searchParams);
 };
 
 export default Login;
