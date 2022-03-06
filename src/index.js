2575c769f1f03414bcbf97569975b3bfc8a49368
Jatin Vaidya <jatin.vaidya@gmail.com>
2022-03-06 18:17:25 +1100

    conn param typo

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const providerConfig = {
 	domain: config.domain,
 	clientId: config.clientId,
 	...(config.audience ? { audience: config.audience } : null),
-	...(config.connection ? { connection: config.conenction } : null),
+	...(config.connection ? { connection: config.connection } : null),
 	redirectUri: window.location.origin,
 	onRedirectCallback,
 };
